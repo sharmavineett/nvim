b9e77d1b6336f32179e8460de548346b4a9a3db2
Sharma, Vineet <69041974+sharmavineett@users.noreply.github.com>
2026-01-01 13:11:50 +1100

    Clean up init.lua and improve documentation
    Removed placeholder comments and added documentation references.

--- init.lua
+++ init.lua
@@ -1,12 +1,18 @@
+-- init.lua
+-- Top-level Neovim entrypoint. This file should be minimal and only bootstrap the rest
+-- of the configuration. Keeping this file small helps startup performance and makes
+-- it obvious where the rest of the config is loaded from.
+
+-- Enable Lua module loader if available (speeds up require() by caching modules).
+-- This is safe to enable on recent Neovim builds and can measurably reduce startup time.
 if vim.loader then vim.loader.enable() end
+
+-- Load the lazy.nvim bootstrapper and plugin loader. This single require call
+-- performs plugin manager installation (if missing), sets runtimepath and then
+-- loads the rest of the user's configuration (globals, options, keymaps, autocmds).
+-- Keeping plugin/bootstrap logic in a dedicated module keeps init.lua tidy.
 require("config.lazy")
--- 
--- hello this is nvim  conffiguration.hello
--- hello this is nvim  conffiguration.hello
--- hello this is nvim  conffiguration.hello
--- hello this is nvim  conffiguration.hello
---
--- hello this is nvim  conffiguration.hello
--- hello this is nvim  conffiguration.hello
---
--- hello this is nvim  conffigurai K ktion.hello
+
+-- The rest of the file previously contained informal/placeholder comments.
+-- Detailed configuration and documentation live in the files under lua/config
+-- and lua/plugins where each setting is documented inline.
